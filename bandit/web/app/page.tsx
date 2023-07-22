d1da778d1b9d7b484344321abcfe38f0001a497d
harperfu6 <beharp8a8@gmail.com>
2023-07-22 15:53:21 +0900

    RSC機能を使ってシミュレーション処理をサーバー側に移行(リクエスト数の削減)

--- bandit/web/app/page.tsx
+++ bandit/web/app/page.tsx
@@ -1,103 +1,19 @@
-"use client"
+"use client";
 
-import init, {
-  AnnealingEpsilonGreedy,
-  AnnealingSoftmax,
-  EpsilonGreedy,
-  Random,
-  Softmax,
-} from "@/pkg/bandit";
-import { useState, useEffect } from "react";
+import init from "@/pkg/bandit";
+import {
+  calcAnnealingEpsilonGreedy,
+  calcAnnealingSoftmax,
+  calcEpsilonGreedy,
+  calcRandom,
+  calcSoftmax,
+} from "./simulation/";
+import { useState, useEffect, useMemo } from "react";
 import { Chart as ChartJS, registerables } from "chart.js";
 import { Line } from "react-chartjs-2";
 ChartJS.register(...registerables);
 
-import { calcAccuracy, getOptArms, AccuracyDict } from "./utils";
-{/* import "./App.css"; */}
-
-type Agent =
-  | Random
-  | EpsilonGreedy
-  | AnnealingEpsilonGreedy
-  | Softmax
-  | AnnealingSoftmax
-  | undefined;
-
-const simulation = async (
-  agent: Agent,
-  arms: number[],
-  sim_num: number,
-  coin_num: number
-) => {
-  console.log(agent);
-  if (agent) {
-    const allRewards = [];
-    const allSelectedArms = [];
-    for (let i = 0; i < sim_num; i++) {
-      agent.call_reset(arms.length);
-      const rewards = [];
-      const selected_arms = [];
-      for (let j = 0; j < coin_num; j++) {
-        const arm = agent.call_select_arm();
-        let reward;
-        if (Math.random() < arms[arm]) {
-          reward = 1;
-        } else {
-          reward = 0;
-        }
-        agent.call_update(arm, reward);
-
-        rewards.push(reward);
-        selected_arms.push(arm);
-      }
-      allRewards.push(rewards);
-      allSelectedArms.push(selected_arms);
-    }
-
-    const accuracyList = calcAccuracy(
-      allSelectedArms,
-      getOptArms(arms, coin_num)
-    );
-
-    return accuracyList;
-  } else {
-    return [];
-  }
-};
-
-const simluationAll = async (
-  simlationList: Agent[],
-  arms: number[],
-  simNum: number,
-  coinNum: number
-) => {
-  const _results = await Promise.all(
-    simlationList.map((agent) => simulation(agent, arms, simNum, coinNum))
-  );
-  const results: AccuracyDict[] = [
-    {
-      name: "random",
-      accuracyList: _results[0],
-    },
-    {
-      name: "epsilonGreedy",
-      accuracyList: _results[1],
-    },
-    {
-      name: "annealingEpsilonGreedy",
-      accuracyList: _results[2],
-    },
-    {
-      name: "softmax",
-      accuracyList: _results[3],
-    },
-    {
-      name: "annealingSoftmax",
-      accuracyList: _results[4],
-    },
-  ];
-  return results;
-};
+import { AccuracyDict } from "./utils";
 
 const makeData = (accracyDict: AccuracyDict) => {
   const accuracyList = accracyDict.accuracyList;
@@ -116,77 +32,83 @@ const makeData = (accracyDict: AccuracyDict) => {
   return data;
 };
 
+const options = {
+  responsive: true,
+  scales: {
+    y: {
+      suggestedMin: 0,
+      suggestedMax: 1.0,
+    },
+  },
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: false,
+      text: "accuracy",
+    },
+  },
+};
+
 const App = () => {
   const [loadWasm, setLoadWasmFlg] = useState(false);
-  const [random, setRandom] = useState<Random>();
-  const [epsilonGreedy, setEpsilonGreedy] = useState<EpsilonGreedy>();
-  const [annealingEpsilonGreedy, setAnnealingEpsilonGreedy] =
-    useState<AnnealingEpsilonGreedy>();
-  const [softmax, setSoftmax] = useState<Softmax>();
-  const [annealingSoftmax, setAnnealingSoftmax] = useState<AnnealingSoftmax>();
-
-  const [isSimulation, setIsSimulation] = useState(false);
   const [accracyDictList, setAccuracyDictList] = useState<AccuracyDict[]>([]);
 
-  const arms = [0.1, 0.1, 0.2, 0.3];
-  const epsilon = 0.1;
-
-  const coinNum = 1000;
-  const simNum = 1000;
+  const calcList = useMemo(
+    () => [
+      calcRandom,
+      calcEpsilonGreedy,
+      calcAnnealingEpsilonGreedy,
+      calcSoftmax,
+      calcAnnealingSoftmax,
+    ],
+    []
+  );
+  const arms = useMemo(() => [0.1, 0.1, 0.2, 0.3], []);
+  const coinNum = 500;
+  const simNum = 500;
 
   useEffect(() => {
-    init()
-      .then(() => setLoadWasmFlg(true))
-      .then(() => setRandom(Random.new(arms.length)))
-      .then(() => setEpsilonGreedy(EpsilonGreedy.new(epsilon, arms.length)))
-      .then(() =>
-        setAnnealingEpsilonGreedy(AnnealingEpsilonGreedy.new(arms.length))
-      )
-      .then(() => setSoftmax(Softmax.new(arms.length)))
-      .then(() => setAnnealingSoftmax(AnnealingSoftmax.new(arms.length)))
-      .then(() => setIsSimulation(true));
-  });
+    init().then(() => setLoadWasmFlg(true));
+  }, []);
 
   useEffect(() => {
     const sim = async () => {
-      const results = await simluationAll(
-        [
-          random,
-          epsilonGreedy,
-          annealingEpsilonGreedy,
-          softmax,
-          annealingSoftmax,
-        ],
-        arms,
-        simNum,
-        coinNum
+      const _results = await Promise.all(
+        calcList.map((calc) => calc(arms, coinNum, simNum))
       );
-      setAccuracyDictList(results);
+
+      const accuracyDictList = [
+        {
+          name: "Random",
+          accuracyList: _results[0],
+        },
+        {
+          name: "EpsilonGreedy",
+          accuracyList: _results[1],
+        },
+        {
+          name: "AnnealingEpsilonGreedy",
+          accuracyList: _results[2],
+        },
+        {
+          name: "Softmax",
+          accuracyList: _results[3],
+        },
+        {
+          name: "AnnealingSoftmax",
+          accuracyList: _results[4],
+        },
+      ];
+      setAccuracyDictList(accuracyDictList);
     };
-    sim();
-  }, [isSimulation]);
+    if (loadWasm) sim();
+  }, [loadWasm, calcList, arms]);
 
   if (!loadWasm) return <div>loading wasm...</div>;
-  if (accracyDictList.length < 5) return <div>loading Agents...</div>;
-
-  const options = {
-    responsive: true,
-    scales: {
-      y: {
-        suggestedMin: 0,
-        suggestedMax: 1.0,
-      },
-    },
-    plugins: {
-      legend: {
-        position: "top" as const,
-      },
-      title: {
-        display: false,
-        text: "accuracy",
-      },
-    },
-  };
+  if (accracyDictList.length < calcList.length)
+    return <div>loading Agents...</div>;
 
   return (
     <>
---
+++ bandit/web/app/simulation/index.tsx
@@ -0,0 +1,123 @@
+import {
+  AnnealingEpsilonGreedy,
+  AnnealingSoftmax,
+  EpsilonGreedy,
+  Random,
+  Softmax,
+} from "@/pkg/bandit";
+import { calcAccuracy, getOptArms, AccuracyDict } from "@/app/utils";
+
+export const calcRandom = async (
+  arms: number[],
+  coinNum: number,
+  simNum: number
+) => {
+  const random = Random.new(arms.length);
+  const accuracyList = await simulation(random, arms, simNum, coinNum);
+  return accuracyList;
+};
+
+export const calcEpsilonGreedy = async (
+  arms: number[],
+  coinNum: number,
+  simNum: number
+) => {
+  const epsilon = 0.1;
+  const epsilonGreedy = EpsilonGreedy.new(epsilon, arms.length);
+  const accuracyList = await simulation(epsilonGreedy, arms, simNum, coinNum);
+  return accuracyList;
+};
+
+export const calcAnnealingEpsilonGreedy = async (
+  arms: number[],
+  coinNum: number,
+  simNum: number
+) => {
+  const annealingEpsilonGreedy = AnnealingEpsilonGreedy.new(arms.length);
+  const accuracyList = await simulation(
+    annealingEpsilonGreedy,
+    arms,
+    simNum,
+    coinNum
+  );
+  return accuracyList;
+};
+
+export const calcSoftmax = async (
+  arms: number[],
+  coinNum: number,
+  simNum: number
+) => {
+  const softmax = Softmax.new(arms.length);
+  const accuracyList = await simulation(softmax, arms, simNum, coinNum);
+  return accuracyList;
+};
+
+export const calcAnnealingSoftmax = async (
+  arms: number[],
+  coinNum: number,
+  simNum: number
+) => {
+  const annealingSoftmax = AnnealingSoftmax.new(arms.length);
+  const accuracyList = await simulation(
+    annealingSoftmax,
+    arms,
+    simNum,
+    coinNum
+  );
+  return accuracyList;
+};
+
+type Agent =
+  | Random
+  | EpsilonGreedy
+  | AnnealingEpsilonGreedy
+  | Softmax
+  | AnnealingSoftmax
+  | undefined;
+
+const simulation = async (
+  agent: Agent,
+  arms: number[],
+  sim_num: number,
+  coin_num: number
+): Promise<number[]> => {
+  console.log(agent);
+  if (agent) {
+    try {
+      const allRewards = [];
+      const allSelectedArms = [];
+      for (let i = 0; i < sim_num; i++) {
+        agent.call_reset(arms.length);
+        const rewards = [];
+        const selected_arms = [];
+        for (let j = 0; j < coin_num; j++) {
+          const arm = agent.call_select_arm();
+          let reward;
+          if (Math.random() < arms[arm]) {
+            reward = 1;
+          } else {
+            reward = 0;
+          }
+          agent.call_update(arm, reward);
+
+          rewards.push(reward);
+          selected_arms.push(arm);
+        }
+        allRewards.push(rewards);
+        allSelectedArms.push(selected_arms);
+      }
+
+      const accuracyList = calcAccuracy(
+        allSelectedArms,
+        getOptArms(arms, coin_num)
+      );
+      return accuracyList;
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  } else {
+    return [];
+  }
+};
